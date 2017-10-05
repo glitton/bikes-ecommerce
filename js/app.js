0257f2e297cf358cf3d3d1a3894930e611fd3ccc
Generosa Litton <generosa.litton@yahoo.com>
2017-10-05 16:57:15 -0700

    refactoring for angular js

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// Citizen Bikes Angular App
+(function(){
+  var app = angular.module('bikes', []);
+
+  app.controller = ('StoreController', function(){
+    this.product = bike
+
+    var bike = {
+      brand : 'Tokyo',
+      frame : 'Steel',
+      speed : '6-Speed',
+      weight: '30lbs.',
+      price : '199', 
+      image : 'img/tokyo.jpg' 
+    };
+
+  });
+
+})();
+
